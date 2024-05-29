d43ae71e18edfc675f92f57f40c7272812a3a196
PrestonMatt <mspreston98@gmail.com>
2024-05-28 22:45:53 -0400

    Added generation of random 1/0 words

--- ARINC429 Simulation/arinc429_voltage_sim.py
+++ ARINC429 Simulation/arinc429_voltage_sim.py
@@ -17,10 +17,20 @@ def main():
     test_all_functions()
 
 def test_all_functions():
-    print("Testing HIGH SPEED 1 bit.")
-    graph_words(create_ARINC429_one_highspeed(0),figtitle = "One High speed Bit")
+    #print("Testing HIGH SPEED 1 bit.")
+    #graph_words(create_ARINC429_one_highspeed(0),figtitle = "One High speed Bit")
+    #print("Testing LOW SPEED 1 bit.")
+    #graph_words(create_ARINC429_one_lowspeed(0),figtitle = "One Low speed Bit")
+    #print("Testing HIGH SPEED 0 bit.")
+    #graph_words(create_ARINC429_zero(0,True),figtitle = "Zero High speed Bit")
+    #print("Testing LOW SPEED 0 bit.")
+    #graph_words(create_ARINC429_zero(0,False),figtitle = "Zero Low speed Bit")
+    print("Creating Random High Speed Word.")
+    graph_words(create_random_word(True))
+    print("Creating Random Low Speed Word.")
+    graph_words(create_random_word(False),tickrate = 50)
 
-def graph_words(word,figtitle = "ARINC 429 Word with Random Bits"):
+def graph_words(word,figtitle = "ARINC 429 Word with Random Bits",tickrate=5):
     ts = word[0]
     vs = word[1]
 
@@ -34,7 +44,7 @@ def graph_words(word,figtitle = "ARINC 429 Word with Random Bits"):
     plt.xlabel("Time in usec")
     plt.ylabel("Voltage")
     plt.title(figtitle)
-    #plt.xticks(np.arange(min(ts),max(ts)+1,5))
+    plt.xticks(np.arange(min(ts),max(ts)+1,tickrate))
     plt.show()
 
 def create_ARINC429_one_highspeed(usec_start):
@@ -89,6 +99,94 @@ def create_ARINC429_one_highspeed(usec_start):
     #print("Voltages: %s", voltages)
 
     return(t_usecs,voltages)
+
+def create_ARINC429_one_lowspeed(usec_start):
+    # Array is in microseconds. This is the LOW SPEED standard
+    # 12 kbps = 0.012 bits per microsecond = 83.33 usec per bit -> round to 84
+    
+    # first [5,15] usec
+    v_rise =[]
+    rise_usecs = random.randint(5,15)
+    # next [40,44] usec
+    v_stab = []
+    stab_usecs = random.randint(40,44)
+    # next [5,15] usec
+    v_fall = []
+    fall_usecs = random.randint(5,15)
+    # final [40,44] usec
+    v_null = []
+    null_usecs = random.randint(40,44)
+
+    this_bit_usec_total = rise_usecs + stab_usecs + fall_usecs + null_usecs
+    #print(rise_usecs, stab_usecs, fall_usecs, null_usecs)
+
+    
+    t_usecs = np.arange(usec_start,
+                         usec_start + this_bit_usec_total,
+                         0.5) # sample every 1/2 microsecond.
+
+    # rise:
+    for step1 in range(rise_usecs * 2): 
+        rise_voltage = random.uniform(0.0,10.0)
+        v_rise.append(rise_voltage)
+    v_rise.sort()
+
+    # high:
+    for step2 in range(stab_usecs * 2): 
+        stable_voltage = random.uniform(9.0,11.0)
+        v_stab.append(stable_voltage)
+
+    # fall:
+    for step3 in range(fall_usecs * 2): 
+        fall_voltage = random.uniform(0.0,10.0)
+        v_fall.append(fall_voltage)
+    v_fall.sort(reverse=True) # need to reverser order this sort
+
+    # null:
+    for step4 in range(null_usecs * 2): 
+        null_voltage = random.uniform(-0.5,0.5)
+        v_null.append(null_voltage)
+
+    voltages = np.concatenate(
+        (v_rise, v_stab, v_fall, v_null)
+    )
+
+    return(t_usecs,voltages)
+
+def create_ARINC429_zero(usec_start,lh_speed):
+    if(lh_speed):
+        ts, vs = create_ARINC429_one_highspeed(usec_start)
+    else:
+        ts, vs = create_ARINC429_one_lowspeed(usec_start)
+
+    new_vs = []
+
+    for voltage in vs:
+        new_voltage = voltage * -1.0
+        new_vs.append(new_voltage)
+
+    zero_voltages = np.array(new_vs)
+
+    return(ts,zero_voltages)
+
+def create_random_word(lh_speed):
+    voltages = np.array([0.0])
+    times = np.array([0.0])
+
+    for x in range(32):
+        one_zero_bool = bool(random.getrandbits(1))
+        if(one_zero_bool): # 1
+            if(lh_speed): # HIGH SPEED
+                ts, vs = create_ARINC429_one_highspeed(times[-1] + 0.5)
+            else: # LOW SPEED
+                ts, vs = create_ARINC429_one_lowspeed(times[-1] + 0.5)
+        else: # 0
+            ts, vs = create_ARINC429_zero(times[-1] + 0.5,lh_speed)
+
+        voltages = np.concatenate((voltages,vs), axis = 0)
+        times = np.concatenate((times, ts), axis = 0)
+
+    return(times,voltages)
 
 if __name__ == "__main__":
     main()
