b173264a248cef1808b25f81f61bdc6b3aaea3a5
mpreston9 <mpreston9@gatech.edu>
2024-05-28 21:55:28 -0400

    add initial file outline for voltage generation within spec

---
+++ ARINC429 Simulation/arinc429_voltage_sim.py
@@ -0,0 +1,19 @@
+import matplotlib as plt
+import numpy as np
+import random
+
+# TX
+# HIGH  (i.e. 1) =>      10.0 V +/- 1.0 V
+# NULL                    0.0 V +/- 0.5 V
+# LOW   (i.e. 0) =>     -10.0 V +/- 1.0 V
+
+# RX
+# HIGH  (i.e. 1) =>     [+ 6.5V, +13.0V]
+# NULL                  [- 2.5V, + 2.5V]
+# LOW   (i.e. 0) =>     [-13.0V, - 6.5V]
+
+def main():
+    print("test")
+
+if __name__ == "__main__":
+    main()
