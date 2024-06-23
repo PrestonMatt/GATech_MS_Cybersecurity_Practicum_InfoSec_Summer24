88018c98f03a89a74c720303737d0d30d1726b6e
PrestonMatt <mspreston98@gmail.com>
2024-06-23 14:38:20 -0400

    Added logging functionality for IDS

--- ARINC429 Simulation/ARINC429_IDS.py
+++ ARINC429 Simulation/ARINC429_IDS.py
@@ -5,22 +5,25 @@ from time import sleep, time
 class arinv429_intrusion_detection_system:
     def __init__(self, bus_speed="low", BUS_CHANNELS=[]):
         # Set bus start time
-        self.bus_start = time()
+        self.start_time = time()
         # Set bus channels.
         self.BUS_CHANNELS = BUS_CHANNELS
         # set ADIRU T/Rx bus speed
         self.bus_speed = bus_speed
 
-        self.communication_chip = lru_rxr()
+        self.communication_chip = lru_rxr(bus_speed=self.bus_speed,
+                                          BUS_CHANNELS=self.BUS_CHANNELS)
 
-        self.rules = {}
-        self.make_default_rules()
+        self.rules = self.make_default_rules()
 
         self.default_filepath = r"C:/ARINC429_IDS/"
 
+        self.buses = {}
+        self.SDI = {}
+
     def make_default_rules(self):
 
-        start_time = time()
+        #start_time = time()
 
         #default_filepath = r"C:/ARINC429_IDS/"
         with open("ARINC429_rules_template.txt","r") as template_fd:
@@ -30,39 +33,44 @@ class arinv429_intrusion_detection_system:
             default_rules_fd.write(template_lines)
         default_rules_fd.close()
 
-        with open(self.default_filepath + r"Alerts/" + f"Alerts_{start_time}.txt", "a") as alerts_fd:
-            alerts_fd.write(f"Starting New Alert Message Recording at {start_time}\n")
+        with open(self.default_filepath + r"Alerts/" + f"Alerts_{self.start_time}.txt", "a") as alerts_fd:
+            alerts_fd.write(f"Starting New Alert Message Recording at {self.start_time}\n")
         alerts_fd.close()
 
-        with open(self.default_filepath + r"Logs/" + f"Logs_{start_time}.txt", "a") as logs_fd:
-            logs_fd.write(f"Starting New Alert Message Recording at {start_time}\n")
+        default_rules = {}
+
+        return default_rules
+
+        with open(self.default_filepath + r"Logs/" + f"Logs_{self.start_time}.txt", "a") as logs_fd:
+            logs_fd.write(f"Starting New Alert Message Recording at {self.start_time}\n")
         logs_fd.close()
 
     def __str__(self):
-        pass
+        return(str(self.rules))
 
     def get_rules(self):
         with open("ARINC429_rules.txt","r") as rules_fd:
             temp_rules = rules_fd.readlines()
         rules_fd.close()
-        # Parse the rules - lines with # are comments
-        # ! indicates a new section
-        # i.e. in the form of:
-        # !Channels
-        # Orange: GPS -> ADIRU
-        # Blue: ADIRU -> FMC
-        # Purple: FMC -> RMS
-        # Purple: FMC -> FAEC 1
-        # Purple: FMC -> FAEC 2
-        # Purple: FMC -> RWnBS
-        # Green: FMC -> RMS
-        # Green: FMC -> FAEC 1
-        # Green: FMC -> FAEC 2
-        # Green: FMC -> RWnBS
-        # !Rules
-        # <alert/log> <channel> <label> <SDI> <data> <SSM> <P> <message>
-        # <alert/log> <channel> <bit[index:index] = "01..10"> <message>
-        # <alert/log> <channel> <label> <BCD/BNR/DISC> <message>
+
+        lines = []
+        for line in temp_rules:
+            if(line[0] == "#" or line == "\n"):
+                continue
+            else:
+                line = line.split("#")[0] # remove endline comments
+                lines.append(line)
+
+        for cleanLine in lines:
+            pass # find rules
 
     def alert(self):
         pass
+
+    def log_words(self, channel_index):
+        with open(self.default_filepath + r"Logs/" + f"Logs_{self.start_time}.txt", "a") as logs_fd:
+            word = self.communication_chip.receive_given_word(channel_index)
+            logs_fd.write(word)
+        logs_fd.close()
+        #bus_channel = self.BUS_CHANNELS[channel_index]
+
