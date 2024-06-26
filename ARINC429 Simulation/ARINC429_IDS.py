40eff7d0b2f8226d155e429584f6cf91b56bf032
PrestonMatt <mspreston98@gmail.com>
2024-06-26 00:12:05 -0400

    Parse rule line function for IDS, UNTESTED

--- ARINC429 Simulation/ARINC429_IDS.py
+++ ARINC429 Simulation/ARINC429_IDS.py
@@ -1,6 +1,8 @@
 from LRU_RX_Helper import arinc429_RX_Helpers as lru_rxr
+from LRU_TX_Helper import arinc429_TX_Helpers as lru_txr
 from BusQueue_Simulator import GlobalBus as ARINC429BUS
 from time import sleep, time
+from queue import Queue
 
 class arinc429_intrusion_detection_system:
     def __init__(self, bus_speed="low", BUS_CHANNELS=[], rules_file = r"C:\Users\mspreston\Desktop\Grad School Work\7 - Summer Semester 2024\Cybersecurity Practicum\GATech_MS_Cybersecurity_Practicum_InfoSec_Summer24\ARINC429 Simulation\ARINC429_rules.txt"):
@@ -26,6 +28,8 @@ class arinc429_intrusion_detection_system:
 
         self.channels = {}
         self.get_channel_connections()
+
+        self.rules = None
 
         #self.rules = self.make_default_rules()
 
@@ -115,22 +119,20 @@ class arinc429_intrusion_detection_system:
             if(line.__contains__("!Channels")):
                 filines = True
 
-
     def get_rules(self):
         with open("ARINC429_rules.txt","r") as rules_fd:
             temp_rules = rules_fd.readlines()
         rules_fd.close()
 
-        #
-
-        lines = []
+        rules_section = False
         for line in temp_rules:
             if(line[0] == "#" or line == "\n"):
                 continue
-            else:
+            if(rules_section):
                 line = line.split("#")[0] # remove endline comments
-                lines.append(line)
-
+                self.handle_ruleline(line)
+            if(line.__contains__("!Rules")):
+                rules_section = True
 
         # Example formats:
         # <alert/log>* <channel>* <label> <SDI> <data> <SSM> <P> <Time> "<message (if alert)>"
@@ -138,9 +140,93 @@ class arinc429_intrusion_detection_system:
         # <alert/log>* <channel>* <bit[index1:index2) = "01..10"> "<message (if alert)>"
         # <alert/log>* <channel>* <label> <BCD/BNR/DISC> "<message (if alert)>"
 
-        for rule_Line in lines:
+    def handle_ruleline(self,line):
+        rule = line.split(" ")
 
-            pass # find rules
+        alert_log = ""
+        channel = ""
+        bitmask = "0"*31
+        parity_check = False
+        time_notate = False
+        message = line.split('"')[1]
+
+        if(not (rule[0].__contains__("alert") or rule.__contains__("log")) ):
+            raise ValueError("Rule must delineate between Alerting or Logging.")
+        else:
+            alert_log = rule[0]
+
+        if(not rule[1] in self.get_channelnames()):
+            raise ValueError("Rule must delineate between Channels.")
+        else:
+            channel = rule[1]
+
+        rulez = Queue()
+        [rulez.put(word) for word in rule]
+
+        octal_flag = True
+        SDI_flag = True
+        data_flag = True
+        SSM_flag = True
+        parity_flag = True
+        time_flag = True
+        message_flag = True
+
+        while(rulez.size() > 0):
+            r = rulez.pop()
+            # octal
+            if(r.__contains__("0o") and octal_flag):
+                octal_flag = False
+                label = int(r,8)
+                bitmask[0:8] = lru_txr.make_label_for_word(label)
+            # SDI
+            elif(SDI_flag and r in self.SDIs):
+                SDI_flag = False
+                bitmask[8:10] = self.SDI[r]
+            # data -> TODO ADD data: to rules semantics
+            # TODO make data have no spaces
+            elif(data_flag and r.__contains__("bit[") or r.__contains__("data:")):
+                data_flag = False
+                if(r.__contains__("bit[")):
+                    # TODO figure out what to do when this contradicts the label/sdi
+                    rz = r.split("=")[1].replace('"','')
+                    index1 = int(r.split("[")[0].split(":")[0])
+                    index2 = int(r.split(":")[1].split(")")[0])
+                    cnt = 0
+                    for char in bitmask[index1:index2]:
+                        try:
+                            if(rz[cnt] != char):
+                                raise ValueError("Bit mask contradicts other flags!!")
+                        except IndexError: # reached past the bit mask.
+                            break
+                    bitmask[index1:index2] = rz
+                    #bitmask += 19 - len(rz)
+                else:
+                    # TODO figure out to encode to DISC, BNR or BCD
+                    pass
+            elif(SSM_flag and (int(data_flag,2) >= 0 or int(data_flag,2) <= 4)):
+                bitmask[29:31] = data_flag
+                SSM_flag = False
+            elif(parity_flag and (r == "C" or r == "I")):
+                parity_check = True if r == "C" else False
+                parity_flag = False
+            elif(time_flag and (r == "time")):
+                time_flag = False
+                time_notate = True
+            #elif(message_flag and r.__contains__('"')):
+            #    mess
+            else:
+                continue
+
+        if(len(bitmask) != 31):
+            raise ValueError("Error in parsing word!")
+
+        self.rules.add((alert_log,channel,bitmask,parity_check,time_notate,message))
+
+    def get_channelnames(self)->list:
+        channelnames = []
+        for chan, value in self.channels.items():
+            channelnames.append(chan)
+        return(channelnames)
 
     def alert(self):
         pass
